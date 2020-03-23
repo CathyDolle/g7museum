b085c0598b1cd9f48614a0eced0eaaa28271be0c
CathyDolle <cathy.dolle@live.fr>
2020-03-23 14:44:45 +0100

    :lipstick:
    cursor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./style/main.styl"
 import * as THREE from "three"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
 
 /**
  * Sizes
@@ -16,9 +16,9 @@ const cursor = {}
 cursor.x = 0
 cursor.y = 0
 
-window.addEventListener('mousemove', (_event) => {
-    cursor.x = _event.clientX / sizes.width - 0.5
-    cursor.y = _event.clientY / sizes.height - 0.5
+window.addEventListener("mousemove", _event => {
+  cursor.x = _event.clientX / sizes.width - 0.5
+  cursor.y = _event.clientY / sizes.height - 0.5
 })
 
 /**
@@ -27,18 +27,38 @@ window.addEventListener('mousemove', (_event) => {
 const scene = new THREE.Scene()
 
 /**
+ * Lights
+ */
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+/**
  * Objects
  */
-const dummy = new THREE.Mesh(
-  new THREE.SphereGeometry(1),
-  new THREE.MeshNormalMaterial()
+
+/**
+ * GroundFloor
+ */
+const groundFloorGroup = new THREE.Group()
+scene.add(groundFloorGroup)
+
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 2.5, 5, 1, 1, 1),
+  new THREE.MeshStandardMaterial({ color: 0xe8c7d6 })
 )
-scene.add(dummy)
+walls.position.y = 1.25
+groundFloorGroup.add(walls)
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  20
+)
 camera.position.z = 8
 scene.add(camera)
 
@@ -49,7 +69,7 @@ const renderer = new THREE.WebGLRenderer({ alpha: true })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setClearAlpha(0)
-const canvasContainer = document.querySelector('.canvas-js')
+const canvasContainer = document.querySelector(".canvas-js")
 canvasContainer.appendChild(renderer.domElement)
 
 /**
@@ -58,7 +78,6 @@ canvasContainer.appendChild(renderer.domElement)
 const cameraControls = new OrbitControls(camera, renderer.domElement)
 cameraControls.zoomSpeed = 0.3
 cameraControls.enableDamping = true
-
 
 /**
  * Resize
@@ -85,3 +104,27 @@ const loop = () => {
 }
 
 loop()
+
+
+/**
+ * CURSOR
+ */
+
+const container = document.getElementById('cursor')
+const cursorCustom = container.querySelector('.cursor-wrapper')
+const pointer = container.querySelector('.pointer')
+let cursorPos = { x: 0, y: 0 }
+let cursorOffset = { x: 0, y: 0 }
+
+function syncCursor(elem = cursorCustom) {
+	const transform = `translate(${cursorPos.x + cursorOffset.x}px, ${cursorPos.y + cursorOffset.y}px)`
+  
+  elem.style.transform = transform
+}
+
+document.addEventListener('mousemove', e => {
+	cursorPos.x = e.clientX
+  cursorPos.y = e.clientY
+  syncCursor()
+  syncCursor(pointer)
+})
