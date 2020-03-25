ded5f50e60841d137ce67d2c9a5f944f2fefddcb
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2020-03-25 23:37:52 +0100

    add plan on box #pixelperfect

--- src/javascript/box.js
+++ src/javascript/box.js
@@ -87,8 +87,59 @@ export default class Box {
     boxBackMesh.rotateX(Math.PI*0.5)
     boxBackMesh.position.set(-0.05,1,-0.95)
     
+
+    const planMaterial = new THREE.MeshBasicMaterial({color:0xf5f5f5, })
     
-    this.group.add(boxBottomMesh, boxTopMesh, boxLeftMesh, boxRightMesh, boxBackMesh)
+    const planBottom = new THREE.PlaneGeometry(2.1,2,2)
+    const planBottomMesh = new THREE.Mesh(planBottom, planMaterial)
+    planBottomMesh.position.set(-0.05,-0.0501,0)
+
+    planBottomMesh.rotateX(Math.PI*0.5)
+
+    
+    const planLeft = new THREE.PlaneGeometry(2,2.1,2)
+    const planLeftMesh = new THREE.Mesh(planLeft, planMaterial)
+    planLeftMesh.rotateY(-Math.PI*0.5)
+    planLeftMesh.position.set(-1.1001,1,0)
+
+    const planRight = new THREE.PlaneGeometry(2,2.1,2)
+    const planRightMesh = new THREE.Mesh(planRight, planMaterial)
+    planRightMesh.rotateY(Math.PI*0.5)
+    planRightMesh.position.set(1.0001,1,0)
+  
+    
+    const planTop = new THREE.PlaneGeometry(2.1,2,2)
+    const planTopMesh = new THREE.Mesh(planTop, planMaterial)
+    planTopMesh.position.set(-0.05,2.0501,0)
+
+    planTopMesh.rotateX(-Math.PI*0.5)
+
+
+    const smallPlanMaterial = new THREE.MeshBasicMaterial({color:0xf5f5f5, })
+    const smallPlanTop = new THREE.PlaneGeometry(2.1,0.1,2)
+    const smallPlanTopMesh = new THREE.Mesh(smallPlanTop, smallPlanMaterial)
+    smallPlanTopMesh.position.set(-0.05,2,1.0001)
+
+    const smallPlanBottom = new THREE.PlaneGeometry(2.1,0.1,2)
+    const smallPlanBottomMesh = new THREE.Mesh(smallPlanBottom, smallPlanMaterial)
+    smallPlanBottomMesh.position.set(-0.05,0,1.0001)
+
+    const smallPlanLeft = new THREE.PlaneGeometry(2,0.1,2)
+    const smallPlanLeftMesh = new THREE.Mesh(smallPlanLeft, smallPlanMaterial)
+    smallPlanLeftMesh.position.set(-1.05,1,1.0001)
+    smallPlanLeftMesh.rotateZ(Math.PI*0.5)
+
+    const smallPlanRight = new THREE.PlaneGeometry(2,0.1,2)
+    const smallPlanRightMesh = new THREE.Mesh(smallPlanRight, smallPlanMaterial)
+    smallPlanRightMesh.position.set(0.95,1,1.0001)
+    smallPlanRightMesh.rotateZ(Math.PI*0.5)
+
+  
+
+  
+
+    
+    this.group.add(boxBottomMesh, boxTopMesh, boxLeftMesh, boxRightMesh, boxBackMesh, planBottomMesh, planLeftMesh, planRightMesh, planTopMesh, smallPlanTopMesh, smallPlanBottomMesh, smallPlanLeftMesh, smallPlanRightMesh)
     
     }
 }
