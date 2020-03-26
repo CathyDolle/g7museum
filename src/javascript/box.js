a4b38a74c97065b466d1b5194bd5af16bb9d7fd7
CathyDolle <cathy.dolle@live.fr>
2020-03-26 14:07:52 +0100

    text update

--- src/javascript/box.js
+++ src/javascript/box.js
@@ -1,4 +1,6 @@
 import * as THREE from "three"
+
+
 import {
   GLTFLoader
 } from "three/examples/jsm/loaders/GLTFLoader.js"
@@ -6,65 +8,35 @@ import {
   DRACOLoader
 } from "three/examples/jsm/loaders/DRACOLoader.js"
 
+import pianoTextSource from './../images/texturePiano.png'
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const pianoTextTexture = textureLoader.load(pianoTextSource)
+
+
 export default class Box {
   constructor() {
     
     this.group = new THREE.Group()
 
-    // const dracoLoader = new DRACOLoader()
-    // dracoLoader.setDecoderPath("/draco/")
-
-    // const gltfLoader = new GLTFLoader()
-    // gltfLoader.setDRACOLoader(dracoLoader)
-    // gltfLoader.load("./models/box/box.gltf", _gltf => {
-    //   this.box = _gltf.scene.children[0]
-    //   this.box.scale.set(2, 2, 2)
-    //   this.box.material = new THREE.MeshStandardMaterial({
-    //     color: 0xffffff,
-    //     metalness: 0.3,
-    //     roughness: 0.8
-    //   })
-    //   this.box.material.side = THREE.DoubleSide
-    //   this.box.castShadow = false
-    //   this.box.receive = true
-    //   this.group.add(this.box)
-    // })
-    // gltfLoader.load("./models/box/box.gltf", _gltf => {
-    //   this.box2 = _gltf.scene.children[0]
-    //   this.box2.scale.set(2, 2, 2)
-    //   this.box2.position.set(0,4.50,0)
-    //   this.box2.material = new THREE.MeshStandardMaterial({
-    //     color: 0xffffff,
-    //     metalness: 0.3,
-    //     roughness: 0.8
-    //   })
-    //   this.box2.material.side = THREE.DoubleSide
-    //   this.box2.castShadow = false
-    //   this.box2.receive = true
-    //   this.group.add(this.box2)
-    // })
-    // gltfLoader.load("./models/box/box.gltf", _gltf => {
-    //   this.box3 = _gltf.scene.children[0]
-    //   this.box3.scale.set(2, 2, 2)
-    //   this.box3.position.set(0,-4.50,0)
-    //   this.box3.material = new THREE.MeshStandardMaterial({
-    //     color: 0xffffff,
-    //     metalness: 0.3,
-    //     roughness: 0.8
-    //   })
-    //   this.box3.material.side = THREE.DoubleSide
-    //   this.box3.castShadow = false
-    //   this.box3.receive = true
-    //   this.group.add(this.box3)
-    // })
     const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff})
 
     const boxBottom = new THREE.BoxGeometry(2, 0.1, 2)
     const boxBottomMesh = new THREE.Mesh(boxBottom, boxMaterial)
 
+    const pianoText = new THREE.Mesh(
+      new THREE.BoxGeometry(2, 0.1, 2),
+      new THREE.MeshStandardMaterial({ color: 0xffffff, map: pianoTextTexture })
+    )
+
+    pianoText.rotateZ(Math.PI*0.5)
+    pianoText.position.set(-1.05,0.95,0)
+
     const boxLeft = new THREE.BoxGeometry(2, 0.1, 2)
     const boxLeftMesh = new THREE.Mesh(boxLeft, boxMaterial)
-
 
 
     const boxTop = new THREE.BoxGeometry(2, 0.1, 2)
@@ -139,7 +111,7 @@ export default class Box {
   
 
     
-    this.group.add(boxBottomMesh, boxTopMesh, boxLeftMesh, boxRightMesh, boxBackMesh, planBottomMesh, planLeftMesh, planRightMesh, planTopMesh, smallPlanTopMesh, smallPlanBottomMesh, smallPlanLeftMesh, smallPlanRightMesh)
+    this.group.add(boxBottomMesh, boxTopMesh, boxLeftMesh, boxRightMesh, boxBackMesh, planBottomMesh, planLeftMesh, planRightMesh, planTopMesh, smallPlanTopMesh, smallPlanBottomMesh, smallPlanLeftMesh, smallPlanRightMesh, pianoText)
     
     }
 }
